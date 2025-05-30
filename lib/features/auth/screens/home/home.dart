ed900d2c79862091711d6b4ee80cfbe359457368
Ariful Haque Mohsin <arifbsns22@gmail.com>
2025-05-30 11:24:15 +0600

    Custom blue background with 2 circular shape and curved edges UI done

--- lib/features/auth/screens/home/home.dart
+++ lib/features/auth/screens/home/home.dart
@@ -1,10 +1,63 @@
 import 'package:flutter/material.dart';
 
+import '../../../../common/widgets/custom_shapes/containers/circular_container.dart';
+import '../../../../common/widgets/custom_shapes/curved_edges/curved_edges.dart';
+import '../../../../utils/constants/colors.dart';
+
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            TCurvedEdgeWidget(
+              child: Container(
+                color: Colors.blue,
+                padding: const EdgeInsets.all(0),
+                child: SizedBox(
+                  height: 400,
+                  child: Stack(
+                    children: [
+                      Positioned(
+                        top: -150,
+                        right: -250,
+                        child: TCircularContainer(
+                          background: TColors.textWhite.withOpacity(0.1),
+                        ),
+                      ),
+                      Positioned(
+                        top: 100,
+                        right: -300,
+                        child: TCircularContainer(
+                          background: TColors.textWhite.withOpacity(0.1),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class TCurvedEdgeWidget extends StatelessWidget {
+  const TCurvedEdgeWidget({
+    super.key,
+    this.child,
+  });
+  final Widget? child;
+  @override
+  Widget build(BuildContext context) {
+    return ClipPath(
+      clipper: TCustomCurvedEdges(),
+      child: child,
+    );
   }
 }
